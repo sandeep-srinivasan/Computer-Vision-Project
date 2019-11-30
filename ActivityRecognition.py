c2e42cd5be73efc863394f9970e1f3fc11892965
Siddharth M <siddharth.m2009@yahoo.in>
2019-11-30 17:17:05 -0500

    Updated activity recignition file

--- ActivityRecognition.py
+++ ActivityRecognition.py
@@ -7,7 +7,9 @@ import glob
 from PIL import Image
 from skimage.color import rgb2gray
 import os
+from scipy import spatial
 
+bg_mapping = {'daria' : 'bg_015.avi' , 'denis' : 'bg_026.avi' , 'eli' : 'bg_062.avi' , 'ido' : 'bg_062.avi' , 'ira' : 'bg_007.avi' , 'lena' : 'bg_026.avi' , 'lyova' : 'bg_046.avi' , 'moshe' : 'bg_070.avi' , 'shahar' : 'bg_079.avi'}
 # Frame grabber
 def frame_grabber(file):
     # video_file = file + '.avi'
@@ -17,16 +19,8 @@ def frame_grabber(file):
     i = 0
     while (cap.isOpened()):
         ret, frame = cap.read()
-        # print(type(frame))
         if ret == False:
             break
-        # if (i < 10):
-        #     cv2.imwrite(file + '-00' + str(i) + '.png', frame)
-        # elif (10 <= i < 100):
-        #     cv2.imwrite(file + '-0' + str(i) + '.png', frame)
-        # else:
-        #     cv2.imwrite(file + '-' + str(i) + '.png', frame)
-        # i += 1
         frames.append(frame)
     cap.release()
     cv2.destroyAllWindows()
@@ -63,7 +57,7 @@ def MHI(image, delta):
         for y in range(row):
             for x in range(column):
                 if (frame[y, x] == 1):
-                    mhi[y, x] = timestamp
+                    mhi[y, x] = timestamp + 1
                 else:
                     if (mhi[y, x] < timestamp - delta):
                         mhi[y, x] = 0
@@ -102,10 +96,8 @@ def MEI_Thresh(mhi):
     mei = mhi > 0
     return mei
 
-# Normalize MHI and MEI
+# Normalize MHI
 def normalize(mhi):
-    # Normalize MHI and MEI,
-
     mhi_n = np.maximum(0, np.divide((mhi - (np.min(mhi[np.nonzero(mhi)]) - 1.0)),
                                     (np.max(mhi[np.nonzero(mhi)]) - (np.min(mhi[np.nonzero(mhi)]) - 1.0))))
 
@@ -136,18 +128,18 @@ def get_temporal_template(file_name , bg_file_name):
     # Threshold for Background Substraction
     thresh = 40
     # Delta value for the number of frames to keep
-    delta = 20
+    delta = 30
     # Grab frames from video of action
     imgFrameData = frame_grabber(file_name)
     imgGrayscaleFrameData = {}
     for i,image in enumerate(imgFrameData):
         grayscale = rgb2gray(image)
         # Convert color image to grayscale image
-        imgGrayscaleFrameData[i] = grayscale * 255
+        imgGrayscaleFrameData[i] = (grayscale * 255).astype(int)
     # Get image of background
     bgFrameData = frame_grabber(bg_file_name)
     # Convert background image from color to grayscale
-    bgImage = rgb2gray(bgFrameData[0]) * 255
+    bgImage = (rgb2gray(bgFrameData[0]) * 255).astype(int)
     # Perform background substraction on the images captured from the videos
     bgSubsImages = background_subtraction(imgGrayscaleFrameData , bgImage, threshold=thresh)
     # Get the MHI from the background subtracted images
@@ -169,49 +161,99 @@ def get_temporal_template(file_name , bg_file_name):
 
 def create_temporal_template_dict():
     tt_dict = {}
-    rootdir = 'Train Dataset'
-    background = 'Backgrounds/bg_007.avi'
+    rootdir = 'Static Train'
+    # rootdir = 'Train Dataset'
+    background = 'Backgrounds'
     for subdir, dir, files in os.walk(rootdir):
-        for d in dir:
-            dirPath = rootdir + '/' + d
-            for file in os.listdir(rootdir + '/' + d):
-                filePath = dirPath + '/' + file
-                temporal_template = get_temporal_template(filePath, background)
-                tt_dict[file] = temporal_template
-                print(file + " processed.")
-                print(temporal_template)
+        for file in files:
+            act_name = file.split('.')[0].split('_')[0]
+            print(act_name)
+            bg_path = background + '/' + bg_mapping[act_name]
+            filePath = rootdir + '/' + file
+            temporal_template = get_temporal_template(filePath, bg_path)
+            tt_dict[file] = temporal_template
+            print(file + " processed.")
+            print(temporal_template)
     print(tt_dict)
-    with open('temporal_templates.txt', 'w') as f:
+    with open('temporal_templates_static_3.txt', 'w') as f:
         f.write(json.dumps(tt_dict))
 
+def get_euclidean_dist(test_template , train_template):
+    return spatial.distance.euclidean(test_template , train_template)
+
 def get_mahalanobis_dist(test_template , train_template):
+    # Method - 1 (Using mahalanobis library)
+    # a = np.asarray(test_template)
+    # b = np.asarray(train_template)
+    # K = np.cov((a , b) , rowvar=False)
+    # k_inv = np.linalg.inv(K)
+    # return spatial.distance.mahalanobis(test_template , train_template , k_inv)
+    # Method - 2 (Using formula in slides)
     test_template = np.asarray(test_template)
     train_template = np.asarray(train_template)
     t = np.vstack((test_template, train_template))
-    print(type(t))
+    # print(type(t))
     k = np.cov(t.T)
-    print(k.shape)
-    md = np.matmul(np.matmul((test_template - train_template).T, (np.linalg.inv(k))), (test_template - train_template))
+    # print(k.shape)
+    mean_temp = np.mean(t , axis=0)
+    # print(mean.shape)
+    md = np.matmul(np.matmul((test_template - mean_temp).T, (np.linalg.pinv(k))), (test_template - mean_temp))
     return (md)
+    # return md
+
+def get_mahalanobis_dist_pinv(test_template , train_template):
+    t = np.array([train_template, test_template]).T
+    k = np.cov(t)
+    md = spatial.distance.mahalanobis(train_template, test_template, np.linalg.pinv(k))
+    return (md)
+
+def get_cosine_dist(test_template , train_template):
+    return spatial.distance.cosine(test_template , train_template)
 
 def predict_activity(file , background , temporal_template_dict):
     pred_temporal_template = get_temporal_template(file, background)
     min_dist = float("inf")
     activity = ""
     for k , v in temporal_template_dict.items():
+        # Using mahalanobis distance
         dist = get_mahalanobis_dist(pred_temporal_template , v)
-        print(k)
-        print(dist)
+        # Using euclidean distance
+        # dist = get_euclidean_dist(pred_temporal_template , v)
+        # Using cosine distance
+        # dist = get_cosine_dist(pred_temporal_template , v)
         if(dist < min_dist):
             min_dist = dist
             activity = k
+    print(min_dist)
     return activity
 
 if __name__ == '__main__':
-    pred_file = 'lyova_run.avi'
-    background = 'Backgrounds/bg_007.avi'
-    with open('temporal_templates.txt' , 'r') as f:
+    # Run below command to train dataset
+    # create_temporal_template_dict()
+    # Prediction Calculation
+    pred_dir = 'Static Test'
+    # pred_dir = 'Test Dataset'
+    background = 'Backgrounds'
+    with open('temporal_templates_static_2.txt' , 'r') as f:
         temporal_template_dict = json.loads(f.read())
     print(temporal_template_dict)
-    result_activity = predict_activity(pred_file , background , temporal_template_dict)
-    print(result_activity)
+    acc_count = 0
+    total = 0
+    for subdir, dir, files in os.walk(pred_dir):
+        for file in files:
+            pred_file_path = pred_dir + '/' + file
+            print(file)
+            act_activity = file.split('.')[0].split('_')[1]
+            act_name = file.split('.')[0].split('_')[0]
+            print(act_activity)
+            bg_path = background + '/' + bg_mapping[act_name]
+            print(bg_path)
+            result_activity = predict_activity(pred_file_path , bg_path , temporal_template_dict)
+            print(result_activity)
+            pred_activity = result_activity.split('.')[0].split('_')[1]
+            print(pred_activity)
+            print("------------")
+            if(act_activity == pred_activity):
+                acc_count += 1
+            total += 1
+    print("Accuracy : ",acc_count / total)
